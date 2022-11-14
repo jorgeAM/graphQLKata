c66549f0d2e9662002d6814c49782b62992867d5
Jorge L <jorge.a@coderhouse.com>
2022-11-14 17:59:32 -0500

    initial files

---
+++ internal/platform/graphql/schema.resolvers.go
@@ -0,0 +1,33 @@
+package graphql
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+)
+
+// Login is the resolver for the login field.
+func (r *mutationResolver) Login(ctx context.Context, input LoginInput) (*User, error) {
+	panic(fmt.Errorf("not implemented: Login - login"))
+}
+
+// SignUp is the resolver for the signUp field.
+func (r *mutationResolver) SignUp(ctx context.Context, input SignUpInput) (*User, error) {
+	panic(fmt.Errorf("not implemented: SignUp - signUp"))
+}
+
+// User is the resolver for the user field.
+func (r *queryResolver) User(ctx context.Context, id *string) (*User, error) {
+	panic(fmt.Errorf("not implemented: User - user"))
+}
+
+// Mutation returns MutationResolver implementation.
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
